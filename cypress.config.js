58bc0067a69b01c421de545feeb9a42c9c349a0b
GauravChandel <gaurav7217444@gmail.com>
2025-03-01 13:28:44 +0530

    Setup the mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+
   e2e: {
     baseUrl: "https://forums.parasoft.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
   },
+  
 });
