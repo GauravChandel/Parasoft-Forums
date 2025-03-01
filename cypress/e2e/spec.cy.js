9f0da9fd5638ee38d370b12a0ffdd077609ed6f9
GauravChandel <gaurav7217444@gmail.com>
2025-03-01 12:31:51 +0530

    Initial cypress setup

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('Parasoft-Forums', () => {
+  beforeEach(() => {
+    cy.visit('https://forums.parasoft.com/')
+  })  
+
+  it('Parasoft-Forums', () => {
+    cy.contains('Parasoft Forums')
+  })  
+})
